5041ff83ebe0dcf39d559151687e3dae46beecd7
Tim Zhu <53026628+lodgepolepines@users.noreply.github.com>
2021-05-09 22:48:42 -0400

    Add files via upload

---
+++ pablo_bot.py
@@ -0,0 +1,41 @@
+import tweepy, time, sys
+import os
+
+
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print(user.name)
+print(user.description)
+print(user.location)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+
+theleftcanwin = "theleftcanwin.txt"
+filename=open(theleftcanwin,'r', errors='replace')
+f=filename.readlines()
+filename.close()
+
+print('Tweeting every 2 hours...')
+
+for line in f:
+    try:
+        api.update_status(line)
+        print(line)
+        time.sleep(7200)#Tweet every 2 hours
+
+    except KeyboardInterrupt:
+        sys.exit("KeyboardInterrupt")
+    except Exception as e:
+        print(e)
+        pass
